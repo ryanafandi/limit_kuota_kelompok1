3e68ecedbd31ffe9182df09ac4f5236f8664e10c
ryanafandi <ryanafandi283@gmail.com>
2026-04-17 09:39:26 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:limit_kuota/src/features/monitoring/network_page.dart';
+import 'package:limit_kuota/src/core/widgets/splash_screen.dart';
 
 void main() {
-  // Jalankan MyApp, bukan langsung Network
-  runApp(const MyApp()); 
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,8 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false, // Opsional: hilangkan banner debug
-      home: Network(), // Network sekarang punya akses ke Directionality dari MaterialApp
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(), 
     );
   }
 }
